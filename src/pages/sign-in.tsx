32b03f6583b8fb51eb8fcc05ca8f259419b237a5
Fernando Saraiva <fpsaraiva@gmail.com>
2024-11-25 22:59:16 -0300

    feat: adicionando estilizacao

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button"
+
 function SignIn() {
-    return <h1>SignIn</h1>
+    return (
+        <>
+            <h1>SignIn</h1>
+            <Button>Clique aqui</Button>
+        </>
+    )
 }
 
 export default SignIn
